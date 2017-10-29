cb12e022948d00f41c154aa862eb668f05a628be
SwapnilSankla <swapnil.sankla@gmail.com>
2017-10-29 14:08:21 +0530

    [Swapnil] : Simplified controller creation logic

--- src/__tests__/controllerfactory.spec.ts
+++ src/__tests__/controllerfactory.spec.ts
@@ -2,31 +2,35 @@ import {} from "jest";
 import { NativeViewNotFoundError } from "../errors";
 
 import { FullMockController } from "../__mocks__/mockcreator";
-import { IController } from "../controller";
+import { CalatravaControllerCtor, IController } from "../controller";
 
 jest.mock("../index");
 // tslint:disable-next-line:ordered-imports
 import { createController } from "../controllerfactory";
 import { Calatrava } from "../index";
+import { INativeView } from "../nativeview";
 
 describe("Calatava.ControllerFactory", () => {
     let mockController: IController;
+    let mockControllerCtor: CalatravaControllerCtor = FullMockController;
+
     beforeEach(() => {
-        mockController = createController(FullMockController);
+        mockController = createController(mockControllerCtor, "key1");
     });
+
     it("#createController should create instance of controller for given type",
      () => {
         expect(mockController)
             .toBeInstanceOf(FullMockController);
     });
-    it("#createController should get nativ eview by convention (controller name)", () => {
+    it("#createController should get native view by the name passed while creating controller", () => {
         expect(Calatrava.NativePlatform.getPage)
-            .toBeCalledWith("FullMockController");
+            .toBeCalledWith("key1");
     });
     it("#constructor should throw error if no corresponding native view", () => {
         Calatrava.NativePlatform.getPage = jest.fn();
         expect(() => {
-            mockController = createController(FullMockController);
+            mockController = createController(mockControllerCtor, "key1");
         }).toThrowError(NativeViewNotFoundError);
     });
 });
